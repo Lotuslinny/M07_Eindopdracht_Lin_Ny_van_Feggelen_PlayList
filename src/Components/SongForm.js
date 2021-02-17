bb2e4ef118ebeb67e00833a7f5113d6be749f72b
Lotuslinny <linny@fegg.nl>
2021-02-17 14:29:10 +0100

    Yes

--- src/Components/SongForm.js
+++ src/Components/SongForm.js
@@ -1,22 +1,13 @@
 import React from "react";
-//import SongFormItems from "./SongFormItems";
+import SongFormItem from "./SongFormItem";
 
 const SongForm = (props) => {
-  {/* <div className="song-form">
-    { SongFormItems className="song-form__items" }
-    <p></p>
-    <h1> {props.items}
-      { addSong={props.addSong} }</h1>
-    { <button className="song-form__button"
-      onClick={props.handleClickForm}
-    >Add Song</button> }
-  </div>
-} */}
   return (
     <div className="song-form-items">
       <table items={props.songs} >
         <tr className="song-form-items__header">
-          <th className="song-form-items__item"
+          <SongFormItem key={props.id} />
+          {/* <th className="song-form-items__item"
           //addSong={props.addSong}
           >Song
         </th>
@@ -31,7 +22,7 @@ const SongForm = (props) => {
           <th className="song-form-items__item"
             addSong={props.addSong}
           >Rating
-        </th>
+        </th> */}
         </tr>
       </table>
     </div>
