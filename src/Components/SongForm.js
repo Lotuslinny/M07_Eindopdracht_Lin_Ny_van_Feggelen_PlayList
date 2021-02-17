b61395b3e82b8fd3c843bc0854e2db0f9a6b8d48
Lotuslinny <linny@fegg.nl>
2021-02-17 12:52:49 +0100

    Whoop

--- src/Components/SongForm.js
+++ src/Components/SongForm.js
@@ -1,15 +1,40 @@
 import React from "react";
-import SongFormItems from "./SongFormItems";
+//import SongFormItems from "./SongFormItems";
 
 const SongForm = (props) => {
-  <div className="song-form">
-    <SongFormItems className="song-form__items"
-      items={props.items}
-      addSong={props.addSong} />
-    <button className="song-form__button"
+  {/* <div className="song-form">
+    { SongFormItems className="song-form__items" }
+    <p></p>
+    <h1> {props.items}
+      { addSong={props.addSong} }</h1>
+    { <button className="song-form__button"
       onClick={props.handleClickForm}
-    >Add Song</button>
+    >Add Song</button> }
   </div>
+} */}
+  return (
+    <div className="song-form-items">
+      <table items={props.songs} >
+        <tr className="song-form-items__header">
+          <th className="song-form-items__item"
+          //addSong={props.addSong}
+          >Song
+        </th>
+          <th className="song-form-items__item"
+          //addSong={props.addSong}
+          >Artist
+        </th>
+          <th className="song-form-items__item"
+            addSong={props.addSong}
+          >Genre
+        </th>
+          <th className="song-form-items__item"
+            addSong={props.addSong}
+          >Rating
+        </th>
+        </tr>
+      </table>
+    </div>
+  )
 }
-
 export default SongForm;
--- src/SongOverview.js
+++ src/SongOverview.js
@@ -1,34 +1,37 @@
 import React from "react";
-//import SongForm from "./Components/SongForm";
+import SongForm from "./Components/SongForm";
 //import SongList from "./Components/SongList";
 console.log("************")
+
 class SongOverview extends React.Component {
-  /* constructor(props) {
+  constructor(props) {
     super(props)
     this.state =
     {
-      songs: []
+      songs: [{ type: "songs", id: 1, title: "hello", artist: "Lionel", genre: "love", rating: "5" }]
     }
-    this.addSong = this.addSong.bind(this);
-    this.handleClickForm = this.handleClickForm.bind(this);
+    //this.addSong = this.addSong.bind(this);
+    //this.handleClickForm = this.handleClickForm.bind(this);
   }
- */
-  //addSong = (song) => {
-  // doe iets om de state aan te passen
-  //console.log(song = "Hello!")
 
-  //handleClickForm = (song) => {
-  // doe iets om de state aan te passen
-  // console.log(song = "Hello!")
+  /* addSong = (song) => {
+    // doe iets om de state aan te passen
+    console.log(song = "Hello!") }*/
 
+  /* handleClickForm = (song) => {
+    // doe iets om de state aan te passen
+    console.log(song = "Hello!")
+  } */
 
   render() {
-    return ("hello")
-    /* <div>
-    <SongForm addSong={this.addSong}
-      handleClickForm={this.handleClickForm} />
-  {/* <SongList songs={this.state.songs} />  }
-    </div > */
+    return (
+      < div >
+        <SongForm items={this.songs} //addSong={this.addSong}
+          /* handleClickForm={this.handleClickForm} */ />
+        {/* <SongList songs={this.state.songs} />   */}
+      </div >
+
+    )
   }
 }
 
