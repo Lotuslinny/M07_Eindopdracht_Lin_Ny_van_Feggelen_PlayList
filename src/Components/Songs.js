2b2ee43469905be48bed8901f3e5f92b6698f573
Lotuslinny <linny@fegg.nl>
2021-02-19 14:35:19 +0100

    ?

--- src/Components/Songs.js
+++ src/Components/Songs.js
@@ -5,13 +5,36 @@ const Songs = (props) => {
   return (
     <div className="songs">
       <table >
-        <tbody /* key={props.id} type={props.type} title={props.title} artist={props.artist} genre={props.genre} rating={props.rating} */>
-
-          <tr className="songs__header" key={props.id} type={props.type} title={props.title} artist={props.artist} genre={props.genre} rating={props.rating}>
-            <th className="songs__title" >{props.title}</th>
-            <th className="songs__artist" key={props.id} ><h1>{props.artist}</h1></th>
-            <th className="songs__genre" key={props.id} >Genre</th>
-            <th className="songs__rating" key={props.id} >Rating</th>
+        <tbody>
+          <tr className="songs__header">
+            <th className="songs__title"
+              key={props.id}
+              type={props.type}
+              title={props.title}
+              artist={props.artist}
+              genre={props.genre}
+              rating={props.rating}>{props.title}</th>
+            <th className="songs__artist"
+              key={props.id}
+              type={props.type}
+              title={props.title}
+              artist={props.artist}
+              genre={props.genre}
+              rating={props.rating} ><h1>{props.artist}</h1></th>
+            <th className="songs__genre"
+              key={props.id}
+              type={props.type}
+              title={props.title}
+              artist={props.artist}
+              genre={props.genre}
+              rating={props.rating} >Genre</th>
+            <th className="songs__rating"
+              key={props.id}
+              type={props.type}
+              title={props.title}
+              artist={props.artist}
+              genre={props.genre}
+              rating={props.rating} >Rating</th>
           </tr>
         </tbody>
       </table>
